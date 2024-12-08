e07afae03f31bf89e8c11270517c43ea6478fe3a
August Gaukstad <august.gaukstad@intility.no>
2024-12-08 01:01:09 +0100

    more struggling with day 6 part 2

--- y_2024/day_06/day6.go
+++ y_2024/day_06/day6.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	file, err := os.Open("input.txt")
+	file, err := os.Open("example.txt")
 	if err != nil {
 		fmt.Println("Error reading file")
 		return
@@ -25,5 +25,11 @@ func main() {
 		}
 		guardMap = append(guardMap, chars)
 	}
-	TraverseMap(guardMap)
+	startPos := FindStartPos(guardMap)
+	traversedMap := TraverseMap(startPos, guardMap)
+	/* fmt.Println("\n::  TRAVERSED MAP  ::")
+	for _, line := range traversedMap.traversedMap {
+		fmt.Println(line)
+	} */
+	TraverseWithObstacles(traversedMap.startPos, traversedMap.traversedMap)
 }
--- y_2024/day_06/part1.go
+++ y_2024/day_06/part1.go
@@ -7,57 +7,102 @@ type Position struct {
 	char int
 }
 
-func TraverseMap(guardMap [][]string) {
+type TraversedMap struct {
+	startPos     Position
+	traversedMap [][]string
+	stuckInLoop  bool
+}
+
+func TraverseMap(startPos Position, guardMap [][]string) TraversedMap {
 	localMap := guardMap
 	traversedPoses := []Position{}
-	startPos := findStartPos(guardMap)
-	fmt.Println("Guard is starting at position: ", startPos)
-	fmt.Println("Position in LocalMap: ", localMap[startPos.line][startPos.char])
+	//fmt.Println("Guard is starting at position: ", startPos)
+	/* fmt.Println("Position in LocalMap: ", localMap[startPos.line][startPos.char])
 	for _, line := range localMap {
 		fmt.Println(line)
-	}
+	} */
 	direction := "^"
 	patrol := true
 	pos := startPos
+	hitManualObstacle := false
+	stuckInLoop := false
 	for patrol {
 		//fmt.Println("Guard is at position: ", pos)
 		/* Checking if guard is moving north */
 		//fmt.Println("Is patrolling? ", patrol)
+		if stuckInLoop {
+			fmt.Println("Guard is stuck in a loop! Breaking out!")
+			break
+		}
 		spot := localMap[pos.line][pos.char]
 		if spot == "X" {
 			//fmt.Println("Guard has been here before!")
 			if direction == "^" {
 				if pos.line > 0 {
-					if localMap[pos.line-1][pos.char] == "#" {
+					nextSpot := localMap[pos.line-1][pos.char]
+					if nextSpot == "#" {
 						direction = ">"
-						/* continue */
+
+					} else if nextSpot == "O" {
+						direction = ">"
+						if !hitManualObstacle {
+							hitManualObstacle = true
+						} else {
+							stuckInLoop = true
+							break
+						}
 					} else {
 						pos.line = pos.line - 1
 					}
 				}
 			} else if direction == ">" {
 				if pos.char < len(localMap[pos.line])-1 {
-					if localMap[pos.line][pos.char+1] == "#" {
+					nextSpot := localMap[pos.line][pos.char+1]
+					if nextSpot == "#" {
 						direction = "v"
 						/* continue */
+					} else if nextSpot == "O" {
+						direction = "v"
+						if !hitManualObstacle {
+							hitManualObstacle = true
+						} else {
+							stuckInLoop = true
+							break
+						}
 					} else {
 						pos.char = pos.char + 1
 					}
 				}
 			} else if direction == "v" {
 				if pos.line < len(localMap)-1 {
-					if localMap[pos.line+1][pos.char] == "#" {
+					nextSpot := localMap[pos.line+1][pos.char]
+					if nextSpot == "#" {
 						direction = "<"
-						/* continue */
+					} else if nextSpot == "O" {
+						direction = "<"
+						if !hitManualObstacle {
+							hitManualObstacle = true
+						} else {
+							stuckInLoop = true
+							break
+						}
 					} else {
 						pos.line = pos.line + 1
 					}
 				}
 			} else if direction == "<" {
 				if pos.char > 0 {
-					if localMap[pos.line][pos.char-1] == "#" {
+					nextSpot := localMap[pos.line][pos.char-1]
+					if nextSpot == "#" {
 						direction = "^"
-						/* continue */
+					} else if nextSpot == "O" {
+						direction = "^"
+						if !hitManualObstacle {
+							hitManualObstacle = true
+						} else {
+							stuckInLoop = true
+							break
+						}
 					} else {
 						pos.char = pos.char - 1
 					}
@@ -68,28 +113,40 @@ func TraverseMap(guardMap [][]string) {
 		if direction == "^" {
 			/* Checking if guard isn't at northmost edge */
 			if pos.line > 0 {
-				fmt.Println("Guard is trying to move north!")
-				fmt.Println("Guard is at position: ", pos)
+				nextSpot := localMap[pos.line-1][pos.char]
+				//fmt.Println("Guard is trying to move north!")
+				//fmt.Println("Guard is at position: ", pos)
 				/* Checking if position has been traversed already */
-				if addToTraversed(traversedPoses, pos.line, pos.char) {
+				if AddToTraversed(traversedPoses, pos.line, pos.char) {
 					traversedPoses = append(traversedPoses, Position{pos.line, pos.char})
 					localMap[pos.line][pos.char] = "X"
 				}
 				/* Checking if next location is open */
-				if localMap[pos.line-1][pos.char] == "." {
-					fmt.Println("Next space is free, moving to: ", pos.line, pos.char-1)
+				if nextSpot == "." {
+					//fmt.Println("Next space is free, moving to: ", pos.line, pos.char-1)
 					localMap[pos.line][pos.char] = "X"
 					pos.line = pos.line - 1
 					/* Checking if next location is blocked */
-				} else if localMap[pos.line-1][pos.char] == "#" {
-					fmt.Println("Next space is blocked, changing direction!")
+				} else if nextSpot == "#" {
+					//fmt.Println("Next space is blocked, changing direction!")
 					direction = ">"
 					localMap[pos.line][pos.char] = "X"
 					//pos.char = pos.char + 1
 					/* Checking if next location has been visited before */
-				} else if localMap[pos.line-1][pos.char] == "X" {
-					fmt.Println("Guard has been here before!")
-					pos.line = pos.line - 1
+				} else if nextSpot == "X" {
+					//fmt.Println("Guard has been here before!")
+					//pos.line = pos.line - 1
+				} else if nextSpot == "O" {
+					if !hitManualObstacle {
+						direction = ">"
+						hitManualObstacle = true
+						fmt.Println("Guard hit manual obstacle!")
+						localMap[pos.line][pos.char] = "X"
+					} else {
+						fmt.Println("Guard hit manual obstacle again!")
+						stuckInLoop = true
+						patrol = false
+					}
 				}
 			} else {
 				fmt.Println("Guard left the area north!")
@@ -101,28 +158,40 @@ func TraverseMap(guardMap [][]string) {
 		if direction == ">" {
 			/* Checking if guard isn't at eastmost edge */
 			if pos.char < len(localMap[pos.line])-1 {
-				fmt.Println("Guard is trying to move east!")
-				fmt.Println("Guard is at position: ", pos)
+				nextSpot := localMap[pos.line][pos.char+1]
+				//fmt.Println("Guard is trying to move east!")
+				//fmt.Println("Guard is at position: ", pos)
 				/* Checking if position has been traversed already */
-				if addToTraversed(traversedPoses, pos.line, pos.char) {
+				if AddToTraversed(traversedPoses, pos.line, pos.char) {
 					traversedPoses = append(traversedPoses, Position{pos.line, pos.char})
 					localMap[pos.line][pos.char] = "X"
 				}
 				/* Checking if next location is open */
-				if localMap[pos.line][pos.char+1] == "." {
-					fmt.Println("Next space is free, moving to: ", pos.line, pos.char+1)
+				if nextSpot == "." {
+					//fmt.Println("Next space is free, moving to: ", pos.line, pos.char+1)
 					localMap[pos.line][pos.char] = "X"
 					//pos.char = pos.char + 1
 					/* Checking if next location is blocked */
-				} else if localMap[pos.line][pos.char+1] == "#" {
-					fmt.Println("Next space is blocked, changing direction!")
+				} else if nextSpot == "#" {
+					//fmt.Println("Next space is blocked, changing direction!")
 					direction = "v"
 					localMap[pos.line][pos.char] = "X"
 					//pos.line = pos.line + 1
 					/* Checking if next location has been visited before */
-				} else if localMap[pos.line][pos.char+1] == "X" {
-					fmt.Println("Guard has been here before!")
-					pos.char = pos.char + 1
+				} else if nextSpot == "O" {
+					if !hitManualObstacle {
+						direction = "v"
+						hitManualObstacle = true
+						fmt.Println("Guard hit manual obstacle!")
+						localMap[pos.line][pos.char] = "X"
+					} else {
+						fmt.Println("Guard hit manual obstacle again!")
+						stuckInLoop = true
+						patrol = false
+					}
+				} else if nextSpot == "X" {
+					//fmt.Println("Guard has been here before!")
+					//pos.char = pos.char + 1
 				}
 			} else {
 				fmt.Println("Guard left the area east!")
@@ -134,28 +203,40 @@ func TraverseMap(guardMap [][]string) {
 		if direction == "v" {
 			/* Checking if guard isn't at southmost edge */
 			if pos.line < len(localMap)-1 {
-				fmt.Println("Guard is trying to move south!")
-				fmt.Println("Guard is at position: ", pos)
+				nextSpot := localMap[pos.line+1][pos.char]
+				//fmt.Println("Guard is trying to move south!")
+				//fmt.Println("Guard is at position: ", pos)
 				/* Checking if position has been traversed already */
-				if addToTraversed(traversedPoses, pos.line, pos.char) {
+				if AddToTraversed(traversedPoses, pos.line, pos.char) {
 					traversedPoses = append(traversedPoses, Position{pos.line, pos.char})
 					localMap[pos.line][pos.char] = "X"
 				}
 				/* Checking if next location is open */
-				if localMap[pos.line+1][pos.char] == "." {
-					fmt.Println("Next space is free, moving to: ", pos.line+1, pos.char)
+				if nextSpot == "." {
+					//fmt.Println("Next space is free, moving to: ", pos.line+1, pos.char)
 					localMap[pos.line][pos.char] = "X"
 					pos.line = pos.line + 1
 					/* Checking if next location is blocked */
-				} else if localMap[pos.line+1][pos.char] == "#" {
-					fmt.Println("Next space is blocked, changing direction!")
+				} else if nextSpot == "#" {
+					//fmt.Println("Next space is blocked, changing direction!")
 					direction = "<"
 					localMap[pos.line][pos.char] = "X"
 					//pos.char = pos.char - 1
 					/* Checking if next location has been visited before */
-				} else if localMap[pos.line+1][pos.char] == "X" {
-					fmt.Println("Guard has been here before!")
-					pos.line = pos.line + 1
+				} else if nextSpot == "O" {
+					if !hitManualObstacle {
+						direction = "<"
+						hitManualObstacle = true
+						fmt.Println("Guard hit manual obstacle!")
+						localMap[pos.line][pos.char] = "X"
+					} else {
+						fmt.Println("Guard hit manual obstacle again!")
+						stuckInLoop = true
+						patrol = false
+					}
+				} else if nextSpot == "X" {
+					//fmt.Println("Guard has been here before!")
+					//pos.line = pos.line + 1
 				}
 			} else {
 				fmt.Println("Guard left the area south!")
@@ -167,28 +248,40 @@ func TraverseMap(guardMap [][]string) {
 		if direction == "<" {
 			/* Checking if guard isn't at westmost edge */
 			if pos.char > 0 {
-				fmt.Println("Guard is trying to move west!")
-				fmt.Println("Guard is at position: ", pos)
+				nextSpot := localMap[pos.line][pos.char-1]
+				//fmt.Println("Guard is trying to move west!")
+				//fmt.Println("Guard is at position: ", pos)
 				/* Checking if position has been traversed already */
-				if addToTraversed(traversedPoses, pos.line, pos.char) {
+				if AddToTraversed(traversedPoses, pos.line, pos.char) {
 					traversedPoses = append(traversedPoses, Position{pos.line, pos.char})
 					localMap[pos.line][pos.char] = "X"
 				}
 				/* Checking if next location is open */
-				if localMap[pos.line][pos.char-1] == "." {
-					fmt.Println("Next space is free, moving to: ", pos.line, pos.char-1)
+				if nextSpot == "." {
+					//fmt.Println("Next space is free, moving to: ", pos.line, pos.char-1)
 					localMap[pos.line][pos.char] = "X"
 					pos.char = pos.char - 1
 					/* Checking if next location is blocked */
-				} else if localMap[pos.line][pos.char-1] == "#" {
-					fmt.Println("Next space is blocked, changing direction!")
+				} else if nextSpot == "#" {
+					//fmt.Println("Next space is blocked, changing direction!")
 					direction = "^"
 					localMap[pos.line][pos.char] = "X"
 					//pos.line = pos.line - 1
 					/* Checking if next location has been visited before */
-				} else if localMap[pos.line][pos.char-1] == "X" {
-					fmt.Println("Guard has been here before!")
-					pos.char = pos.char - 1
+				} else if nextSpot == "O" {
+					if !hitManualObstacle {
+						direction = "^"
+						hitManualObstacle = true
+						fmt.Println("Guard hit manual obstacle!")
+						localMap[pos.line][pos.char] = "X"
+					} else {
+						fmt.Println("Guard hit manual obstacle again!")
+						stuckInLoop = true
+						patrol = false
+					}
+				} else if nextSpot == "X" {
+					//fmt.Println("Guard has been here before!")
+					//pos.char = pos.char - 1
 				}
 			} else {
 				fmt.Println("Guard left the area west!")
@@ -199,8 +292,8 @@ func TraverseMap(guardMap [][]string) {
 		}
 
 	}
-	fmt.Printf("\n::TRAVERSED MAP::\n")
-	total_x := 0
+
+	/* total_x := 0
 	for _, line := range localMap {
 		for _, char := range line {
 			if char == "X" {
@@ -209,10 +302,11 @@ func TraverseMap(guardMap [][]string) {
 		}
 		fmt.Println(line)
 	}
-	fmt.Println("Traversed poses: ", total_x)
+	fmt.Println("Traversed poses: ", total_x) */
+	return TraversedMap{startPos, localMap, stuckInLoop}
 }
 
-func findStartPos(guardMap [][]string) Position {
+func FindStartPos(guardMap [][]string) Position {
 	for i, line := range guardMap {
 		for j, pos := range line {
 			if pos == "^" {
@@ -223,7 +317,8 @@ func findStartPos(guardMap [][]string) Position {
 	return Position{-1, -1}
 }
 
-func addToTraversed(traversedPoses []Position, line, char int) bool {
+func AddToTraversed(traversedPoses []Position, line, char int) bool {
+
 	newPos := Position{line, char}
 	for _, pos := range traversedPoses {
 		if pos == newPos {
