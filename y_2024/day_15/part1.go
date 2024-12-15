13418b59adf2612cbc3fc276c864a1002b0e5380
August Gaukstad <august.gaukstad@intility.no>
2024-12-15 17:06:16 +0100

    struggle continue

--- y_2024/day_15/part1.go
+++ y_2024/day_15/part1.go
@@ -51,9 +51,26 @@ func move2(room []string, dir int, objects [][2]int) []string {
 	return room
 }
 
+<<<<<<< HEAD
 func run2(room []string, sx, sy int, inst byte) ([]string, int, int) {
 	var objects [][2]int // To track parts of wide boxes
 	var x, y int         // New robot position after move
+=======
+func MoveBotWiderMap(posMap map[Pos]string, bot Bot, width, height int) {
+	fmt.Println("Moving bot in wider map")
+	for _, mov := range bot.Movements {
+		fmt.Println("\nBot pos: ", bot.Pos)
+		fmt.Println("Bot mov: ", mov)
+		nextPos := Pos{bot.Pos.Char + mov.Dir[1], bot.Pos.Line + mov.Dir[0]}
+		fmt.Println("Next pos: ", nextPos)
+		if posMap[nextPos] == "." {
+			fmt.Println("Next pos: ", nextPos, " value: ", posMap[nextPos])
+			fmt.Println("Next pos is empty, moving bot!")
+			posMap[bot.Pos] = "."
+			posMap[nextPos] = "@"
+			bot.Pos = nextPos
+			//printMap(posMap, width, height)
+>>>>>>> d7e96cc (struggle continue)
 
 	switch inst {
 	case '^': // Move up
@@ -150,11 +167,236 @@ func run2(room []string, sx, sy int, inst byte) ([]string, int, int) {
 
 func countSumBoxesCoordsX2(room []string) int {
 	sum := 0
+<<<<<<< HEAD
 	for y, line := range room {
 		for x := 0; x < len(line); x++ {
 			if line[x] == '[' {
 				sum += x + y*100
 			}
+=======
+	for boxPos, value := range posMap {
+		if value == "[" {
+			gps := 0
+			//fmt.Println("Box at pos: ", boxPos)
+			gps = (100 * boxPos.Char) + boxPos.Line
+			sum += gps
+		}
+	}
+	fmt.Println("Sum of GPS coordinates: ", sum)
+}
+
+func MoveBot(posMap map[Pos]string, bot Bot, width, height int) {
+	fmt.Println("Moving bot")
+	for _, mov := range bot.Movements {
+		fmt.Println("\nBot pos: ", bot.Pos)
+		fmt.Println("Bot mov: ", mov)
+		nextPos := Pos{bot.Pos.Char + mov.Dir[1], bot.Pos.Line + mov.Dir[0]}
+		fmt.Println("Next pos: ", nextPos)
+		if posMap[nextPos] == "." {
+			fmt.Println("Next pos: ", nextPos, " value: ", posMap[nextPos])
+			fmt.Println("Next pos is empty, moving bot!")
+			posMap[bot.Pos] = "."
+			posMap[nextPos] = "@"
+			bot.Pos = nextPos
+			//printMap(posMap, width, height)
+
+		} else if posMap[nextPos] == "#" {
+			fmt.Println("Next pos is wall")
+
+		} else if posMap[nextPos] == "O" {
+			fmt.Println("Next pos is a box!")
+			posMap = CheckIfBoxCanBeMoved(posMap, mov, &bot)
+
+		} else {
+			fmt.Println("Something is wrong with next pos...")
+			fmt.Printf("Next pos: %v, value: %v\n", nextPos, posMap[nextPos])
+		}
+		//printMap(posMap, width, height)
+	}
+	printMap(posMap, width, height)
+	sum := 0
+	//fmt.Println("PosMap: ", posMap)
+	for boxPos, value := range posMap {
+		if value == "O" {
+			gps := 0
+			//fmt.Println("Box at pos: ", boxPos)
+			gps = (100 * boxPos.Char) + boxPos.Line
+			sum += gps
+		}
+	}
+	fmt.Println("Sum of GPS coordinates: ", sum)
+}
+
+func CheckIfWideBoxCanBeMoved(posMap map[Pos]string, dir Direction, bot *Bot) map[Pos]string {
+	fmt.Println("Checking if wide box can be moved...")
+
+	// Starting position
+	origPos := bot.Pos
+	nextPos := Pos{bot.Pos.Char + dir.Dir[1], bot.Pos.Line + dir.Dir[0]} // Position where the robot is pushing
+	boxesMap := make(map[Pos]string)
+
+	// ** Handle North-South (Vertical) Movement **
+	if dir.Name == "N" || dir.Name == "S" {
+		line := nextPos.Line
+		for {
+			// Register all box parts in the current line
+			currentLine := make(map[Pos]string)
+			for left := nextPos.Char; posMap[Pos{left, line}] == "[" || posMap[Pos{left, line}] == "]"; left-- {
+				currentLine[Pos{left, line}] = posMap[Pos{left, line}]
+			}
+			for right := nextPos.Char + 1; posMap[Pos{right, line}] == "[" || posMap[Pos{right, line}] == "]"; right++ {
+				currentLine[Pos{right, line}] = posMap[Pos{right, line}]
+			}
+
+			// If the line is blocked or empty, stop
+			isBlocked := false
+			for pos := range currentLine {
+				belowPos := Pos{pos.Char, line + dir.Dir[0]}
+				if posMap[belowPos] == "#" || posMap[belowPos] != "." {
+					isBlocked = true
+				}
+			}
+			if isBlocked || len(currentLine) == 0 {
+				break
+			}
+
+			// Add current line to the boxesMap
+			for pos, char := range currentLine {
+				boxesMap[pos] = char
+			}
+
+			// Move to the next line
+			line += dir.Dir[0]
+		}
+	}
+
+	// ** Handle East-West (Horizontal) Movement **
+	if dir.Name == "W" || dir.Name == "E" {
+		for posMap[nextPos] == "[" || posMap[nextPos] == "]" {
+			boxesMap[nextPos] = posMap[nextPos]
+			nextPos = Pos{nextPos.Char + dir.Dir[1], nextPos.Line + dir.Dir[0]} // Traverse horizontally
+		}
+	}
+
+	fmt.Println("Boxes map: ", boxesMap)
+
+	// Check if all new positions for the box parts are clear
+	canMove := true
+	newBoxPositions := make(map[Pos]string)
+	for boxPos, value := range boxesMap {
+		newPos := Pos{boxPos.Char + dir.Dir[1], boxPos.Line + dir.Dir[0]}
+		if posMap[newPos] != "." {
+			canMove = false
+			fmt.Printf("Obstacle detected at %v\n", newPos)
+			break
+		}
+		newBoxPositions[newPos] = value
+	}
+
+	if canMove {
+		fmt.Println("Wide boxes can be moved!")
+		// Clear old positions
+		for boxPos := range boxesMap {
+			posMap[boxPos] = "."
+		}
+		// Update new positions
+		for pos, char := range newBoxPositions {
+			posMap[pos] = char
+		}
+		// Move the robot
+		posMap[origPos] = "."
+		bot.Pos = Pos{bot.Pos.Char + dir.Dir[1], bot.Pos.Line + dir.Dir[0]}
+		posMap[bot.Pos] = "@"
+	} else {
+		fmt.Println("Wide box cannot be moved. Obstacle detected.")
+	}
+
+	return posMap
+}
+
+func CheckIfWideBoxCanBeMoved2(posMap map[Pos]string, dir Direction, bot *Bot) map[Pos]string {
+	fmt.Println("Checking if wide box can be moved...")
+	origPos := bot.Pos
+	nextPos := Pos{bot.Pos.Char + dir.Dir[1], bot.Pos.Line + dir.Dir[0]}
+	origNextPos := nextPos
+	boxesMap := make(map[Pos]string)
+	if dir.Name == "N" {
+		line := nextPos.Char
+		fmt.Println("Line: ", line)
+		boxesMap[nextPos] = posMap[nextPos]
+		leftPos := Pos{nextPos.Char - 1, nextPos.Line}
+		rightPos := Pos{nextPos.Char + 1, nextPos.Line}
+		for posMap[leftPos] == "[" || posMap[leftPos] == "]" {
+			leftPos = Pos{leftPos.Char - 1, leftPos.Line}
+			boxesMap[leftPos] = posMap[leftPos]
+		}
+		for posMap[rightPos] == "[" || posMap[rightPos] == "]" {
+			rightPos = Pos{rightPos.Char + 1, rightPos.Line}
+			boxesMap[rightPos] = posMap[rightPos]
+		}
+		line--
+		for !checkNextLine(posMap, boxesMap, line) {
+			boxesMap = findNextLineWideBoxes(posMap, boxesMap)
+			line--
+		}
+		fmt.Println("Boxes map: ", boxesMap)
+
+	}
+	if dir.Name == "W" || dir.Name == "E" {
+		for posMap[nextPos] == "[" || posMap[nextPos] == "]" {
+			nextPos = Pos{nextPos.Char + dir.Dir[1], nextPos.Line + dir.Dir[0]}
+			boxesMap[nextPos] = posMap[nextPos]
+		}
+	}
+	for pos, value := range boxesMap {
+		fmt.Println("Pos: ", pos, " value: ", value)
+	}
+	if posMap[nextPos] == "." {
+		fmt.Println("Wide boxes can be moved!")
+		i := len(boxesMap)
+		for boxPos := range boxesMap {
+			if i%2 == 0 {
+				posMap[boxPos] = "]"
+			} else {
+				posMap[boxPos] = "["
+			}
+			i--
+		}
+		bot.Pos = origNextPos
+		posMap[origPos] = "."
+		posMap[bot.Pos] = "@"
+	}
+	return posMap
+}
+
+func findNextLineWideBoxes(posMap map[Pos]string, boxesMap map[Pos]string) map[Pos]string {
+	fmt.Println("Finding next line wide boxes...")
+	//nextPos := Pos{botPos.Char + dir.Dir[1], botPos.Line + dir.Dir[0]}
+
+	//boxesMap[nextPos] = posMap[nextPos]
+	for pos := range boxesMap {
+		leftPos := Pos{pos.Char - 1, pos.Line}
+		rightPos := Pos{pos.Char + 1, pos.Line}
+		for posMap[leftPos] == "[" || posMap[leftPos] == "]" {
+			boxesMap[leftPos] = posMap[leftPos]
+			leftPos = Pos{leftPos.Char - 1, leftPos.Line}
+		}
+		for posMap[rightPos] == "[" || posMap[rightPos] == "]" {
+			boxesMap[rightPos] = posMap[rightPos]
+			rightPos = Pos{rightPos.Char + 1, rightPos.Line}
+		}
+	}
+
+	return boxesMap
+}
+
+func checkNextLine(posMap map[Pos]string, boxesMap map[Pos]string, line int) bool {
+	fmt.Println("Checking next line...")
+	nextLineBlocked := false
+	for pos := range boxesMap {
+		if posMap[Pos{pos.Char, line}] == "#" {
+			nextLineBlocked = true
+>>>>>>> d7e96cc (struggle continue)
 		}
 	}
 	return sum
